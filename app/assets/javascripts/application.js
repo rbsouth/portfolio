5279d510e5db00c86d22a0b7c9bf47fcf15d2328
Reid South <rbsouth@gmail.com>
2018-04-12 22:01:23 -0600

    setup complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require parsley
+//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
