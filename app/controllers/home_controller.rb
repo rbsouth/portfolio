19f3884b74b716796b63af9c22d58d291df145de
Reid South <rbsouth@gmail.com>
2018-04-13 02:16:51 -0600

    profile tab photo, info, skills, link to resume added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
   	@myName = 'Reid South'
+  	@resumeLink = 'https://docs.google.com/document/d/1sMCqqe30Rc9oiXKotg7rWWAiBELFwagnAlnIPdw86sI/edit?usp=sharing'
   end
 
 end
