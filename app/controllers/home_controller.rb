a76349ab16acb90ea067fcf16b82b940677c08e3
Reid South <rbsouth@gmail.com>
2018-04-16 23:37:51 -0600

    contact me styled, social links added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
 
   def index
+  	@nycdaLink = 'https://nycda.com/courses/web-development-intensive'
   	@myName = 'Reid South'
   	@myGithub = 'https://github.com/rbsouth'
   	@myLinkedin = 'https://www.linkedin.com/in/reid-south/'
+  	@myTwitter = 'https://twitter.com/reid_south'
   	@resumeLink = 'https://docs.google.com/document/d/1sMCqqe30Rc9oiXKotg7rWWAiBELFwagnAlnIPdw86sI/edit?usp=sharing'
   	@eatinGithub = 'https://github.com/rbsouth/eatin-ror.git'
   	@eatinDemo = 'https://reid-eatin.herokuapp.com/'
